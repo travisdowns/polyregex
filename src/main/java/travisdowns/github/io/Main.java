fd3c70dcfcf50e8df2041f97c772f9df7baad446
tdowns <travis.downs@gmail.com>
2019-04-07 01:10:15 -0500

    main function

--- src/main/java/travisdowns/github/io/Main.java
+++ src/main/java/travisdowns/github/io/Main.java
@@ -1,10 +1,12 @@
 package travisdowns.github.io;
 
-
-public class Main
-{
-    public static void main( String[] args )
-    {
-//        System.out.println(Parser.postfix("( 5 + 7 ) * 2"));
+public class Main {
+    public static void main(String[] args) {
+        String pattern = args[0];
+        for (int i = 1; i < args.length; i++) {
+            if (Matcher.matches(pattern, args[i])) {
+                System.out.println(args[i]);
+            }
+        }
     }
 }
